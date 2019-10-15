1569f7dc49765760bd66971e12b99a54301c8a0c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:51:51 +0200

    Conteggio tempo di esecuzione del thred

--- App_Threading/MainWindow.xaml.cs
+++ App_Threading/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -29,15 +30,16 @@ namespace App_Threading
         private void Btn_Task_Click(object sender, RoutedEventArgs e)
         {
             //DoWork();
+            lbl_Risultato.Content = "";
             Task.Factory.StartNew(DoWork);
 
         }
 
         private void DoWork()
         {
-            for (int i = 0; i <= 1000; i++)
+            for (int i = 0; i <= 10000; i++)
             {
-                for (int j = 0; j <= 1000; j++)
+                for (int j = 0; j <= 100000; j++)
                 {
 
                 }
@@ -49,6 +51,29 @@ namespace App_Threading
         private void AggiornaInterfaccia()
         {
             lbl_Risultato.Content = "finito";
+        }
+
+        private void Btn_conta_Click(object sender, RoutedEventArgs e)
+        {
+            //DoCount();
+            Task.Factory.StartNew(DoCount);
+        }
+
+        private void DoCount()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    Dispatcher.Invoke(()=>AggiornaInterfaccia(j));
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void AggiornaInterfaccia(int j)
+        {
+            lbl_conta.Content = j.ToString();
         }
     }
 }
